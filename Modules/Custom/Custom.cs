25171c7760329ea8689a8827944584d436dfe606
Aux <justin.gentry@auxesis.tv>
2016-05-16 11:57:23 -0500

    Initial stuff for CustomModule, fixed ~doge not deleting images.

---
+++ Modules/Custom/Custom.cs
@@ -0,0 +1,22 @@
+﻿using Discord;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dogey.Modules.Custom
+{
+    public class Custom
+    {
+        public static bool Exists(Server guild)
+        {
+            return false;
+        }
+
+        public static void Create(Server guild)
+        {
+
+        }
+    }
+}
---
+++ Modules/Custom/Types/ReturnableType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dogey.Modules.Custom.Types
+{
+    public class ReturnableType
+    {
+        public string Image = "Image";
+        public string Text = "Text";
+        public string Sound = "Sound";
+    }
+}
