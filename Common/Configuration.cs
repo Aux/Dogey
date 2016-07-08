5c127f180487558467c88298bf66a1dde2f8ea80
Auxes <justin.gentry@auxesis.tv>
2016-07-08 15:22:11 -0500

    Added Avatar Rotation

--- Common/Configuration.cs
+++ Common/Configuration.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,10 +14,16 @@ namespace Dogey.Common
         public string GoogleToken { get; set; }
         public char Prefix { get; set; }
         public string Playing { get; set; }
+        public string Avatar { get; set; }
 
         public Configuration FromFile(string file)
         {
             return JsonConvert.DeserializeObject<Configuration>(System.IO.File.ReadAllText(file));
+        }
+
+        public void ToFile(string file)
+        {
+            File.WriteAllText(file, JsonConvert.SerializeObject(this, Formatting.Indented));
         }
 
         public string ToJson()
