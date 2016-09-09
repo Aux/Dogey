522e0245df3eec639e844fb039e0ff57e9c17602
Aux <justin.gentry@auxesis.tv>
2016-09-09 10:17:30 -0500

    Added temporary mod log

--- Dogey/Modules/AdminModule/ModGroup.cs
+++ Dogey/Modules/AdminModule/ModGroup.cs
@@ -4,6 +4,7 @@ using Discord.WebSocket;
 using Dogey.Attributes;
 using Dogey.Enums;
 using Dogey.Extensions;
+using Dogey.Models;
 using Dogey.Tools;
 using System;
 using System.Collections.Generic;
@@ -28,29 +29,35 @@ namespace Dogey.Modules.AdminModule
 
         private async Task OnUserUnbanned(IUser u, IGuild g)
         {
-            var log = await g.GetLogChannelAsync();
-
-            if (log != null)
-            {
-
-            }
+            await GuildLog.SendLog(u, g, ModAction.Unban);
         }
 
         private async Task OnUserBanned(IUser u, IGuild g)
         {
-            var log = await g.GetLogChannelAsync();
-
-            if (log != null)
-            {
-
-            }
+            await GuildLog.SendLog(u, g, ModAction.Ban);
         }
 
         [Command("reason")]
         [Description("Give a reason for a moderator action.")]
         public async Task Reason(IUserMessage msg, int number, [Remainder]string reason)
         {
-            await Task.Delay(1);
+            var guild = (msg.Channel as IGuildChannel).Guild;
+            var log = await GuildLog.UpdateLog(msg.Author, guild, number, reason);
+
+            if (log.MsgId != null)
+            {
+                var channel = await guild.GetLogChannelAsync();
+                var message = (await channel.GetMessageAsync((ulong)log.MsgId)) as IUserMessage;
+                await message.ModifyAsync(x =>
+                {
+                    int index = message.Content.LastIndexOf('\n');
+                    string newmsg = message.Content.Substring(0, index);
+
+                    x.Content = newmsg + $"\n**Reason:** {reason}\n**Moderator:** {msg.Author}";
+                });
+            }
+
+            await msg.Channel.SendMessageAsync(":thumbsup:");
         }
     }
 }
