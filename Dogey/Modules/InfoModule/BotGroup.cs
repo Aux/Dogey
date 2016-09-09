d3a55a4153a01dd2211c8f5b138e55ba1cd0e912
Auxe <jascrod@yahoo.com>
2016-09-08 22:00:55 -0500

    Fixed info emote

--- Dogey/Modules/InfoModule/BotGroup.cs
+++ Dogey/Modules/InfoModule/BotGroup.cs
@@ -44,7 +44,7 @@ namespace Dogey.Modules.InfoModule
                 prefix = Globals.Config.DefaultPrefix;
             }
             
-            string message = $"Hello! I'm {self} :auxHappy:. You can get information about my commands by using `{prefix}help`. " +
+            string message = $"Hello! I'm {self} <:auxHappy:213686501089738752>. You can get information about my commands by using `{prefix}help`. " +
                 "To report bugs or request features you can join my guild here: https://discord.gg/0sjlWZiGRvRNZAqx";
 
             await msg.Channel.SendMessageAsync(message);
@@ -63,8 +63,8 @@ namespace Dogey.Modules.InfoModule
                 $"  Library: Discord.Net ({DiscordConfig.Version})",
                 $"  Runtime: {RuntimeInformation.FrameworkDescription} {RuntimeInformation.OSArchitecture}",
                 $"   Uptime: {(DateTime.Now - Process.GetCurrentProcess().StartTime)}",
-                $"Heap Size: {Math.Round(GC.GetTotalMemory(true) / (1024.0 * 1024.0), 2).ToString()}",
-                $"  Latency: {_client.Latency}ms",
+                $"Heap Size: {Math.Round(GC.GetTotalMemory(true) / (1024.0 * 1024.0), 2)} MB",
+                $"  Latency: {_client.Latency} MS",
                 $"   Guilds: {(await _client.GetGuildSummariesAsync()).Count()}",
                 $" Channels: {(await _client.GetGuildsAsync()).Select(async g => await g.GetChannelsAsync()).Count()}",
                 $"    Users: {(await _client.GetGuildsAsync()).Select(async g => await g.GetUsersAsync()).Count()}",
@@ -72,73 +72,6 @@ namespace Dogey.Modules.InfoModule
             };
 
             await Utility.SendMessage(msg, string.Join("\n", infomsg));
-        }
-
-        [Module("botinfo"), Name("Info")]
-        public class SubCommands
-        {
-            private DiscordSocketClient _client;
-
-            public SubCommands(DiscordSocketClient client)
-            {
-                _client = client;
-            }
-
-            [Command("owner")]
-            [Alias("own", "o")]
-            [Description("Get Dogey's owner.")]
-            public async Task Owner(IUserMessage msg)
-            {
-                await Utility.SendMessage(msg, $"My owner is **Auxesis#8522** (158056840402436096).");
-            }
-
-            [Command("library")]
-            [Alias("lib", "l")]
-            [Description("Get the library Dogey is currently running on.")]
-            public async Task Library(IUserMessage msg)
-            {
-                await Utility.SendMessage(msg, $"I am currently running Discord.Net ({DiscordConfig.Version})");
-            }
-
-            [Command("runtime")]
-            [Alias("run", "r")]
-            [Description("Get the runtime Dogey is currently using.")]
-            public async Task Runtime(IUserMessage msg)
-            {
-                await Utility.SendMessage(msg, $"I am currently running {RuntimeInformation.FrameworkDescription} {RuntimeInformation.OSArchitecture}.");
-            }
-
-            [Command("uptime")]
-            [Alias("online", "up", "u")]
-            [Description("Get how long Dogey has been online.")]
-            public async Task Uptime(IUserMessage msg)
-            {
-                await Utility.SendMessage(msg, (DateTime.Now - Process.GetCurrentProcess().StartTime).ToString());
-            }
-
-            [Command("heap")]
-            [Alias("memory", "mem", "m")]
-            [Description("Get how much memory Dogey is currently using.")]
-            public async Task Heap(IUserMessage msg)
-            {
-                await Utility.SendMessage(msg, Math.Round(GC.GetTotalMemory(true) / (1024.0 * 1024.0), 2).ToString());
-            }
-
-            [Command("latency")]
-            [Alias("lag", "ping", "p")]
-            [Description("Get Dogey's current ping.")]
-            public async Task Latency(IUserMessage msg)
-            {
-                await Utility.SendMessage(msg, $"{_client.Latency}ms");
-            }
-
-            [Command("guilds")]
-            [Alias("channels", "users")]
-            [Description("Get info about the guilds Dogey is connected to.")]
-            public async Task Guilds(IUserMessage msg)
-            {
-                await Task.Delay(1);
-            }
         }
     }
 }
