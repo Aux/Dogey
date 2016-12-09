ba7a7c9040428214d93791771cfcbe927bcd40a7
Aux <justin.gentry@auxesis.tv>
2016-12-09 16:34:27 -0600

    Begin add LogContext

---
+++ Dogey/Common/Contexts/LogContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dogey
+{
+    public class LogContext : DbContext
+    {
+        public DbSet<DiscordMessage> Messages { get; set; }
+
+
+    }
+}
---
+++ Dogey/Common/Models/Logs/DiscordMessage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dogey
+{
+    public class DiscordMessage
+    {
+        public DateTime Timestamp { get; set; }
+        public ulong? GuildId { get; set; }
+        public ulong? ChannelId { get; set; }
+        public ulong AuthorId { get; set; }
+        public string Content { get; set; }
+    }
+}
