124f66383a38c06fa1d1a83e96e39a77d4d29950
Aux <justin.gentry@auxesis.tv>
2017-02-14 16:22:44 -0600

    Add YoutubeModule

---
+++ src/Dogey/Modules/YoutubeModule.cs
@@ -0,0 +1,31 @@
+﻿using Discord.Commands;
+using System.Threading.Tasks;
+
+namespace Dogey.Modules
+{
+    [Group("youtube"), Alias("yt")]
+    public class YoutubeModule : ModuleBase<SocketCommandContext>
+    {
+        [Command]
+        public Task BaseAsync()
+            => new HelpModule().HelpAsync(Context, "youtube");
+
+        [Command("search")]
+        public Task SearchAsync([Remainder]string query)
+        {
+            return Task.CompletedTask;
+        }
+
+        [Command("channel")]
+        public Task ChannelAsync([Remainder]string query)
+        {
+            return Task.CompletedTask;
+        }
+
+        [Command("playlist")]
+        public Task PlaylistAsync([Remainder]string query)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
