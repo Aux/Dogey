e307c14a1ea79ebb7eb704e3df5db3a001edd93c
Aux <justin.gentry@auxesis.tv>
2017-02-15 16:13:10 -0600

    Readd Redis project

---
+++ src/Dogey.Redis/Entities/Logs/RedisDiscordMessage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Dogey.Redis.Entities.Logs
+{
+    public class RedisDiscordMessage : RedisEntity<long>, IDiscordMessage
+    {
+        public DateTime CreatedAt { get; set; }
+        public ulong? GuildId { get; set; }
+        public ulong ChannelId { get; set; }
+        public ulong MessageId { get; set; }
+        public ulong AuthorId { get; set; }
+        public string Name { get; set; }
+        public string Content { get; set; }
+        public DateTime? DeletedAt { get; set; }
+        public bool? IsDeleted { get; set; }
+    }
+}
---
+++ src/Dogey.Redis/Entities/RedisEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace Dogey.Redis
+{
+    public class RedisEntity<T> : IEntity<T>
+    {
+        public T Id { get; set; }
+    }
+}
