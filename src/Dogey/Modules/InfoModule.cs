c57c868720b1d1391c789092b99bfba7ed1e65b2
Aux <justin.gentry@auxesis.tv>
2017-02-15 14:54:17 -0600

    Add InfoModule

---
+++ src/Dogey/Modules/InfoModule.cs
@@ -0,0 +1,31 @@
+﻿using Discord.Commands;
+using System.Threading.Tasks;
+
+namespace Dogey.Modules
+{
+    [Group("info")]
+    public class InfoModule : ModuleBase<SocketCommandContext>
+    {
+        [Command]
+        public Task BaseAsync()
+            => new HelpModule().HelpAsync(Context, "info");
+
+        [Command("summary")]
+        public Task SummaryAsync()
+        {
+            return Task.CompletedTask;
+        }
+
+        [Command("performance")]
+        public Task PerformanceAsync()
+        {
+            return Task.CompletedTask;
+        }
+
+        [Command("activity")]
+        public Task ActivityAsync()
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
