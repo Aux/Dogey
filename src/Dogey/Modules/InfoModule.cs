bf15e4158677cfadc9e8c9fc624f11cbb6732af9
Aux <justin.gentry@auxesis.tv>
2017-02-25 22:06:29 -0600

    Add InfoModule.PerformanceAsync

--- src/Dogey/Modules/InfoModule.cs
+++ src/Dogey/Modules/InfoModule.cs
@@ -1,4 +1,8 @@
-﻿using Discord.Commands;
+﻿using Discord;
+using Discord.Commands;
+using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 
 namespace Dogey.Modules
@@ -7,10 +11,17 @@ namespace Dogey.Modules
     public class InfoModule : ModuleBase<SocketCommandContext>
     {
         private CommandService _service;
+        private Process _process;
 
         public InfoModule(CommandService service)
         {
             _service = service;
+        }
+
+        protected override void BeforeExecute()
+        {
+            _process = Process.GetCurrentProcess();
+
         }
 
         [Command]
@@ -26,7 +37,21 @@ namespace Dogey.Modules
         [Command("performance")]
         public Task PerformanceAsync()
         {
-            return Task.CompletedTask;
+            var builder = new EmbedBuilder();
+            builder.ThumbnailUrl = Context.Client.CurrentUser.GetAvatarUrl();
+            builder.Title = "Performance Information for Dogey";
+
+            var uptime = (DateTime.Now - _process.StartTime);
+
+            var desc = $"**Uptime:** {uptime.Days} day {uptime.Hours} hr {uptime.Minutes} min {uptime.Seconds} sec\n" +
+                       $"**Library:** Discord.Net ({DiscordConfig.Version})\n" +
+                       $"**OS:** {RuntimeInformation.OSDescription} {RuntimeInformation.OSArchitecture}\n" +
+                       $"**Framework:** {RuntimeInformation.FrameworkDescription}\n" +
+                       $"**Memory Usage:** {Math.Round(GC.GetTotalMemory(true) / (1024.0 * 1024.0), 2)}mb\n" +
+                       $"**Latency:** {Context.Client.Latency}ms\n";
+
+            builder.Description = desc;
+            return ReplyAsync("", embed: builder);
         }
 
         [Command("activity")]
