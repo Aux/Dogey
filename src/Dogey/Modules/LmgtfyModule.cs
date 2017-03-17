8dfd0742c5aa80bffd13b91239a35c8ee695a137
Aux <justin.gentry@auxesis.tv>
2017-03-17 10:01:12 -0500

    Added lmgtfy

---
+++ src/Dogey/Modules/LmgtfyModule.cs
@@ -0,0 +1,21 @@
+﻿using Discord.Commands;
+using System;
+using System.Threading.Tasks;
+
+namespace Dogey.Modules
+{
+    [Group("lmgtfy")]
+    public class LmgtfyModule : ModuleBase<SocketCommandContext>
+    {
+        private const string _lmgtfyUrl = "http://lmgtfy.com/?q=";
+
+        [Command]
+        [Remarks("For when someone does not quite know how to use google.")]
+        public Task LmgtfyAsync([Remainder]string query)
+        {
+            string cleanQuery = Uri.EscapeDataString(query);
+            string url = _lmgtfyUrl + cleanQuery;
+            return ReplyAsync(url);
+        }
+    }
+}
