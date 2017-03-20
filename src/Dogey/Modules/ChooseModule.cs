810a32a7351cf9d85f18b8dd6d2c90f8097bf923
Aux <justin.gentry@auxesis.tv>
2017-03-19 20:29:02 -0500

    Add choose command

---
+++ src/Dogey/Modules/ChooseModule.cs
@@ -0,0 +1,22 @@
+﻿using Discord.Commands;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dogey.Modules
+{
+    public class ChooseModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("choose")]
+        [Remarks("Select an option at random")]
+        public Task ChooseAsync(params string[] options)
+        {
+            var r = new Random();
+
+            int selectedIndex = r.Next(0, options.Count());
+            string selected = options.ElementAt(selectedIndex);
+
+            return ReplyAsync(selected);
+        }
+    }
+}
