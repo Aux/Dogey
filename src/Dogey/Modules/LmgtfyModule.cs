79f5dfaaef13821783b7565a94d6a55d2515e3d1
Aux <justin.gentry@auxesis.tv>
2017-04-19 15:53:31 -0500

    Add lmfgtfy

--- src/Dogey/Modules/LmgtfyModule.cs
+++ src/Dogey/Modules/LmgtfyModule.cs
@@ -4,17 +4,26 @@ using System.Threading.Tasks;
 
 namespace Dogey.Modules
 {
-    [Group("lmgtfy")]
     public class LmgtfyModule : ModuleBase<SocketCommandContext>
     {
         private const string _lmgtfyUrl = "http://lmgtfy.com/?q=";
+        private const string _lmfgtfyUrl = "http://lmfgtfy.com/?q=";
 
-        [Command]
+        [Command("lmgtfy")]
         [Remarks("For when someone does not quite know how to use google.")]
         public Task LmgtfyAsync([Remainder]string query)
         {
             string cleanQuery = Uri.EscapeDataString(query);
             string url = _lmgtfyUrl + cleanQuery;
+            return ReplyAsync(url);
+        }
+
+        [Command("lmfgtfy")]
+        [Remarks("For when someone does not quite know how to use google.")]
+        public Task LmfgtfyAsync([Remainder]string query)
+        {
+            string cleanQuery = Uri.EscapeDataString(query);
+            string url = _lmfgtfyUrl + cleanQuery;
             return ReplyAsync(url);
         }
     }
