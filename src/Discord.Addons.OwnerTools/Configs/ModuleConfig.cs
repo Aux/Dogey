ef54dba5f1831c2c134b4e5be37eb3cff45c2e1f
Aux <justin.gentry@auxesis.tv>
2017-04-19 15:52:46 -0500

    Remove ownertools project

--- src/Discord.Addons.OwnerTools/Configs/ModuleConfig.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Discord.Addons.OwnerTools
-{
-    public class ModuleConfig
-    {
-        public string Name { get; set; } = "OwnerTools";
-        public string Summary { get; set; } = "Tools for the owner of this bot to manage settings";
-        public string Remarks { get; set; } = null;
-    }
-}
