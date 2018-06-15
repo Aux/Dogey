feba615105152482c5cc0361b71c6c7186346784
Aux <justin@akitaux.com>
2018-06-14 21:12:01 -0500

    Add choosemodule

---
+++ src/Dogey/Modules/ChooseModule.cs
@@ -0,0 +1,43 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dogey.Modules
+{
+    [RequireEnabled]
+    public class ChooseModule : DogeyModuleBase
+    {
+        private readonly Random _random;
+
+        public ChooseModule(Random random)
+        {
+            _random = random;
+        }
+
+        [Command("choose")]
+        public async Task ChooseAsync(params string[] choices)
+        {
+            int selectedIndex = _random.Next(0, choices.Count());
+            var selected = choices.ElementAt(selectedIndex);
+
+            await ReplyAsync(selected);
+        }
+
+        [Command("choose")]
+        public async Task ChooseAsync(int amount, params string[] choices)
+        {
+            var selected = new List<string>();
+            var selectedIndices = new List<int>();
+
+            for (int i = 0; i <= amount; i++)
+            {
+                int selectedIndex = _random.Next(0, choices.Count());
+                selected.Add(choices.ElementAt(selectedIndex));
+            }
+
+            await ReplyAsync(string.Join(", ", selected));
+        }
+    }
+}
