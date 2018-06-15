82d5b67455fda6eba21b80cce18daac203638c33
Aux <justin@akitaux.com>
2018-06-14 21:14:09 -0500

    Add sudomodule

---
+++ src/Dogey/Modules/SudoModule.cs
@@ -0,0 +1,28 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Threading.Tasks;
+
+namespace Dogey.Modules
+{
+    [RequireOwner]
+    [RequireEnabled]
+    public class SudoModule : DogeyModuleBase
+    {
+        private readonly CommandHandler _handler;
+        private readonly IServiceProvider _provider;
+
+        public SudoModule(CommandHandler handler, IServiceProvider provider)
+        {
+            _handler = handler;
+            _provider = provider;
+        }
+
+        [Command("sudo")]
+        public async Task SudoAsync(SocketUser user, [Remainder]string command)
+        {
+            var context = new DogeyCommandContext(Context.Client, Context.Message, user);
+            await _handler.ExecuteAsync(context, _provider, command);
+        }
+    }
+}
