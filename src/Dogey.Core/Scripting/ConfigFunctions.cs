4176be7e04351d9670f6fab95920ab19f96c5de3
Aux <justin@akitaux.com>
2019-03-07 13:20:15 -0600

    Add blacklisted config options

--- src/Dogey.Core/Scripting/ConfigFunctions.cs
+++ src/Dogey.Core/Scripting/ConfigFunctions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.Extensions.Configuration;
 using Scriban.Runtime;
 
@@ -6,12 +7,22 @@ namespace Dogey.Scripting
 {
     public class ConfigFunctions : ScriptObject
     {
+        public static readonly string[] BlacklistedOptions = new[]
+        {
+            "discord:token"
+        };
+
         public ConfigFunctions(IConfiguration config)
         {
             var configFuncs = new ScriptObject();
-            configFuncs.Import("getstring", new Func<string, string>((path) => config[path]));
-            configFuncs.Import("getnumber", new Func<string, float?>((path) => 
+            configFuncs.Import("getstring", new Func<string, string>((path) => 
             {
+                if (BlacklistedOptions.Any(x => x == path)) return null;
+                return config[path];
+            }));
+            configFuncs.Import("getnumber", new Func<string, float?>((path) =>
+            {
+                if (BlacklistedOptions.Any(x => x == path)) return null;
                 if (float.TryParse(config[path], out float value))
                     return value;
                 return null;
