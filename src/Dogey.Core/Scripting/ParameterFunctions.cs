0c071beee4fe347910675d2926f644d8b651b1ab
Aux <justin@akitaux.com>
2019-03-11 23:02:41 -0500

    Add parameter importing

---
+++ src/Dogey.Core/Scripting/ParameterFunctions.cs
@@ -0,0 +1,17 @@
+﻿using Scriban.Runtime;
+using System;
+
+namespace Dogey.Scripting
+{
+    public class ParameterFunctions : ScriptObject
+    {
+        public ParameterFunctions(string[] parameters)
+        {
+            var paramFunc = new ScriptObject();
+            paramFunc.SetValue("value", string.Join(" ", parameters), true);
+            paramFunc.Import("get", new Func<int, string>((i) => parameters?[i]));
+
+            SetValue("params", paramFunc, true);
+        }
+    }
+}
