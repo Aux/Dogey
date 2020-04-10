df4b1a8de991f7f6a0203ded0a91c1bb91c129d6
Aux <justin@akitaux.com>
2020-04-10 17:44:41 -0500

    Add remaining startup info

--- src/Dogey/Program.cs
+++ src/Dogey/Program.cs
@@ -5,6 +5,6 @@ namespace Dogey
     class Program
     {
         static Task Main(string[] args)
-            => new Startup().StartAsync();
+            => new Startup(args).StartAsync();
     }
 }
--- src/Dogey/Startup.cs
+++ src/Dogey/Startup.cs
@@ -1,17 +1,68 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using Dogey.Logging;
+using Dogey.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace Dogey
 {
     public class Startup
     {
+        private IConfiguration _config;
+        private LocaleService _locales;
 
+        public Startup(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Path.Combine(AppContext.BaseDirectory, "common"))
+                .AddYamlFile("config.yml")
+                .AddCommandLine(args);
+            _config = builder.Build();
+            _locales = new LocaleService(_config);
+        }
 
         public async Task StartAsync()
         {
-            await Task.Delay(0);
+            var services = new ServiceCollection();
+            ConfigureServices(services);
+            var provider = services.BuildServiceProvider();
+
+            var discord = provider.GetRequiredService<DiscordSocketClient>();
+            await discord.LoginAsync(Discord.TokenType.Bot, _config["discord:token"]);
+            await discord.StartAsync();
+
+            var commands = provider.GetRequiredService<CommandService>();
+            await commands.AddModulesAsync(Assembly.GetExecutingAssembly(), provider);
+
+            provider.GetRequiredService<ILoggerFactory>().AddProvider(new BotLoggerProvider(_config));
+            provider.GetRequiredService<LoggingService>().Start();
+
+            await Task.Delay(-1);
+        }
+
+        private void ConfigureServices(ServiceCollection services)
+        {
+            services
+                .AddSingleton(new DiscordSocketClient(new DiscordSocketConfig
+                {
+                    MessageCacheSize = 50,
+                    LogLevel = Discord.LogSeverity.Verbose
+                }))
+                .AddSingleton(new CommandService(new CommandServiceConfig
+                {
+                    CaseSensitiveCommands = false,
+                    IgnoreExtraArgs = false
+                }))
+                .AddSingleton(_config)
+                .AddSingleton(_locales)
+                .AddSingleton<LoggingService>()
+                .AddLogging();
         }
     }
 }
