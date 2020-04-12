1327ebbdad3111135df9ca377704b654245c0631
Aux <justin@akitaux.com>
2020-04-12 16:44:15 -0500

    Update locales

--- src/Dogey.Core/Logging/LoggingOptions.cs
+++ src/Dogey.Core/Logging/LoggingOptions.cs
@@ -5,7 +5,7 @@
         public bool UseColorOutput { get; } = true;
         public bool UseRelativeOutput { get; } = true;
         public int MaxFileSizeKb { get; } = 5000;
-        public string OutputDirectory { get; } = "logs";
+        public string OutputDirectory { get; } = "common/logs";
         public string DateTimeFormat { get; } = "yyyy-MM-dd";
     }
 }
